4ef6b394da82d1f66ebb9eebdf901afd9fedc173
Hermes <hermes.tessaro@gmail.com>
2019-08-20 00:22:48 -0300

    main activity tentativa de navdrawer

--- app/src/main/java/com/example/academia/MainActivity.kt
+++ app/src/main/java/com/example/academia/MainActivity.kt
@@ -1,22 +1,104 @@
 package com.example.academia
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.academia.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.toolbar.toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
     lateinit var db: DatabaseHelper
+    lateinit var drawerLayout: DrawerLayout
     private val DB_NAME = "database.db"
     private val DB_VERSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.navDrawer)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        drawerLayout.addDrawerListener(object:DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(p0: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDrawerSlide(p0: View, p1: Float) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDrawerClosed(p0: View) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDrawerOpened(p0: View) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
 
         db = DatabaseHelper(applicationContext, DB_NAME, null, DB_VERSION)
 
         //TODO: SYNC
 
+        val profName = intent.getStringExtra("PROF")
+
     }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            1 -> Toast.makeText(this, "alunos", Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "exercicios", Toast.LENGTH_SHORT).show()
+            3 -> Toast.makeText(this, "treino", Toast.LENGTH_SHORT).show()
+            4 -> Toast.makeText(this, "profs", Toast.LENGTH_SHORT).show()
+            5 -> Toast.makeText(this, "aparelhos", Toast.LENGTH_SHORT).show()
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+
+
+    companion object {
+        fun start(context: Context, prof: String): Intent {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra("PROF", prof)
+            return intent
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/academia/SelectTrainer.kt
+++ app/src/main/java/com/example/academia/SelectTrainer.kt
@@ -1,12 +1,28 @@
 package com.example.academia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_selecttrainer.*
 
 class SelectTrainer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selecttrainer)
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.prof_list_test, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        prof_list.adapter = adapter
+
+        button_prof.setOnClickListener {
+            val intent = MainActivity.start(this, prof_list.selectedItem.toString())
+            startActivity(intent)
+        }
     }
+
+
+
+
 }
