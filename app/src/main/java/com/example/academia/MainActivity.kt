ef1580524d743b326e3ca303c4460ddfe8729db5
Hermes <hermes.tessaro@gmail.com>
2019-08-20 16:18:07 -0300

    Toolbar and Nav View inflating error fixed; NavDrawer mark I

--- app/src/main/java/com/example/academia/MainActivity.kt
+++ app/src/main/java/com/example/academia/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -14,48 +15,26 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.example.academia.R
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.toolbar.toolbar
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+class MainActivity : AppCompatActivity(){
 
     lateinit var db: DatabaseHelper
     lateinit var drawerLayout: DrawerLayout
+    lateinit var toolbar_layout: androidx.appcompat.widget.Toolbar
     private val DB_NAME = "database.db"
     private val DB_VERSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setSupportActionBar(toolbar)
-        drawerLayout = findViewById(R.id.navDrawer)
-
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        drawerLayout.addDrawerListener(object:DrawerLayout.DrawerListener{
-            override fun onDrawerStateChanged(p0: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onDrawerSlide(p0: View, p1: Float) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onDrawerClosed(p0: View) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onDrawerOpened(p0: View) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
-
+        initView()
 
         db = DatabaseHelper(applicationContext, DB_NAME, null, DB_VERSION)
 
@@ -65,20 +44,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        when(p0.itemId){
-            1 -> Toast.makeText(this, "alunos", Toast.LENGTH_SHORT).show()
-            2 -> Toast.makeText(this, "exercicios", Toast.LENGTH_SHORT).show()
-            3 -> Toast.makeText(this, "treino", Toast.LENGTH_SHORT).show()
-            4 -> Toast.makeText(this, "profs", Toast.LENGTH_SHORT).show()
-            5 -> Toast.makeText(this, "aparelhos", Toast.LENGTH_SHORT).show()
+    fun initView(){
+        toolbar_layout = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.navDrawer)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar_layout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.nav_item_one -> Toast.makeText(this, "alunos", Toast.LENGTH_LONG).show()
+                R.id.nav_item_two -> Toast.makeText(this, "exercicios", Toast.LENGTH_LONG).show()
+                R.id.nav_item_three -> Toast.makeText(this, "treino", Toast.LENGTH_LONG).show()
+                R.id.nav_item_four -> Toast.makeText(this, "profs", Toast.LENGTH_LONG).show()
+                R.id.nav_item_five -> Toast.makeText(this, "aparelhos", Toast.LENGTH_LONG).show()
+            }
+            
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
-
-        drawerLayout.closeDrawer(GravityCompat.START)
-
-        return true
     }
-
 
     override fun onBackPressed() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
